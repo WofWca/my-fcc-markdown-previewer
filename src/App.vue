a4743c308db3c6a715c042e796d7f6320b202c2d
WofWca <wofwca@gmail.com>
2019-03-16 13:49:11 +0800

    Add markdown compilation

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,35 @@
     <textarea
       id="editor"
       v-model="markdown"
+      @input="updateMarkup"
     >
       Some default text
     </textarea>
-    <section id="preview">
-      {{ markup }}
+    <section id="preview" v-html="markup">
     </section>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import marked from 'marked';
 import defaultMarkdown from './placeholderMarkdown';
+
+marked.setOptions({
+  gfm: true,
+  silent: true,
+});
 
 @Component
 export default class App extends Vue {
   private markdown: string = defaultMarkdown;
-  private markup!: string;
+  private markup: string = '';
+  private updateMarkup() {
+    this.markup = marked(this.markdown);
+  }
+  private created() {
+    this.updateMarkup();
+  }
 }
 </script>
 
