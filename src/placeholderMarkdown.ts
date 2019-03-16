bc017e4b6c4460f24e6f0644834cb2c743c76fcd
WofWca <wofwca@gmail.com>
2019-03-16 14:55:55 +0800

    Change placeholder Markdown

--- src/placeholderMarkdown.ts
+++ src/placeholderMarkdown.ts
@@ -1,41 +1,47 @@
 export default `
-Marked - Markdown Parser
-========================
+# Welcome to my Vue Markdown Previewer!
 
-[Marked] lets you convert [Markdown] into HTML.  Markdown is a simple text format whose goal is to be very easy to \
-read and write, even when not converted to HTML.  This demo page will let you type anything you like and see how it \
-gets converted.  Live.  No more waiting around.
+## This is a sub-heading...
+### And here's some other cool stuff:
 
-How To Use The Demo
--------------------
-s
-1. Type in stuff on the left.
-2. See the live updates on the right.
+Heres some code, \`<div></div>\`, between 2 backticks.
 
-That's it.  Pretty simple.  There's also a drop-down option in the upper right to switch between various views:
+\`\`\`
+// this is multi-line code:
 
-- **Preview:**  A live display of the generated HTML as it would render in a browser.
-- **HTML Source:**  The generated HTML before your browser makes it pretty.
-- **Lexer Data:**  What [marked] uses internally, in case you like gory stuff like this.
-- **Quick Reference:**  A brief run-down of how to format things using markdown.
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
 
-Why Markdown?
--------------
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
 
-It's easy.  It's not overly bloated, unlike HTML.  Also, as the creator of [markdown] says,
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
 
-> The overriding design goal for Markdown's
-> formatting syntax is to make it as readable
-> as possible. The idea is that a
-> Markdown-formatted document should be
-> publishable as-is, as plain text, without
-> looking like it's been marked up with tags
-> or formatting instructions.
+And if you want to get really crazy, even tables:
 
-Ready to start writing?  Either start changing stuff on the left or
-[clear everything](/demo/?text=) with a simple click.
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
 
-[Marked]: https://github.com/markedjs/marked/
-[Markdown]: http://daringfireball.net/projects/markdown/
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
 
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want!
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![Vue Logo w/ Text](https://vuejs.org/images/logo.png)
 `;
